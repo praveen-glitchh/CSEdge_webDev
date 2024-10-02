9a9ad1c89ea6830e8017026032da7727151d53b6
praveen-glitchh <praveendn012@gmail.com>
2024-10-02 18:42:47 +0530

    codes_uploaded

---
+++ script.js
@@ -0,0 +1,128 @@
+// alert("HELLO");
+// console.log("WELCOME");
+// a = "JavaScript";
+// b ="javascript";
+// c = a==b;
+// let, var, const
+// let a = "WebDev";
+// a = "Front-end";
+// var b=9.6;
+// var b= 8.566;
+// const c = 78;
+
+// let d;
+// var e;
+// d=8;
+// e=222;
+// const k = 8.999;
+// u = undefined;
+// j = null;
+// data-types
+// primitive - number, string, boolean, undefined, null, bigInt, Symbol;
+// non-primitive - objects -> arrays, functions;
+
+
+
+// let a = 8;
+// let b = 9;
+// let d ="9";
+
+// e = d==b;
+// c = d===b; 
+
+// x = d!=b;
+// z = d!==b;
+
+// (exp1)?"exp2":"exp3";
+// let v = (a>10)?console.log('GREATER'):console.log('lESSER');
+
+// let j = (b>10)?"no":"yes";
+
+// string operators
+// - strctn(str1/a,str2/b), strcmp(),strcpy(dest/d,source/a), strrev(a),strlen();
+// let d;
+// a = "John";
+// b = "Cena";
+// z = a +" "+b;
+// x=a;
+
+// let arr =[1,4,75,7,7.98,6.55,"btr"];
+// console.log(window.document);
+// console.dir(window.document);
+
+// DOM In Js
+// Documnet object model(DOM)
+
+//         window
+//           | 
+//        document
+//           |
+//         HTML
+//       |       | 
+//     1.Head, 2.Body
+//    1.meta, meta, link, text, title...
+//    2.garbage, main,.... script.
+
+// console.log(document.body);
+// console.dir(window);
+
+// Manipulation 
+// Selectors
+// let text = document.getElementsByTagName("p");
+// console.log(text);
+// text.innerHTML ="DOM for day 13 in our session and this is changed one";
+
+// Query selectors
+// document.querySelector("#id/.class/tag")
+// let text = document.querySelectorAll("p");
+// console.log(text);
+// text.innerHTML = "DOM for day 13 in our session and this is changed one";
+
+// let text = document.querySelectorAll("p");
+// console.log(text);
+// text[3].innerHTML="Query Selector";
+
+
+
+// Element properties
+// 1.tagName
+// 2.innerHTML
+// 3.innerText
+// 4.textContent
+
+// let text = document.querySelector("#txt");
+// console.log(text.innerText);
+// console.log(text.innerHTML);
+// console.log(text.textContent);
+
+// console.log(text.innerHTML);
+// text.innerText = "Swaroop";
+// console.log(text.innerHTML);
+// text.innerHTML ="<h1>Goutham</h1>"
+// text.innerHTML="iohkigkgv";
+
+
+// let devo =document.getElementById("devo");
+// console.log(devo)
+// console.dir(devo)
+
+// devo.childNodes[0].innerText = "one-1";
+
+
+
+let arr = [1,2,3,4,5];
+
+// for(let i = 0;i<7;i++){
+//     console.log(arr[i]);
+// }
+// for(let ele of arr){
+//     console.log(ele);
+// }
+// for(let ele in arr){
+//     console.log(ele);
+// }
+// arr.forEach((value,index,arr)=>{
+//     console.log(value,index,arr);
+// })
+
+
